31f8b5b2b154d871886acb950c807b5923d31d96
leo-grnd <leogarnaud84@gmail.com>
2026-07-26 00:51:23 +0200

    chore(release): bump version to 1.1.2 and update versionCode to 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -161,8 +161,8 @@ android {
         applicationId = "eu.dotshell.pelo"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.1.1"
+        versionCode = 8
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
